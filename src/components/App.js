2a2235841f3c24648cbd2bbba9378c2c81fdff57
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-16 12:36:26 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,7 @@ class App extends React.Component {
                 onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
